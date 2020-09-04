646a422ac3fb16e98f6ec111be5dfb003e936d43
deanoamor <toonoob4@gmail.com>
2020-09-04 21:47:03 +0700

    Tolong bantu ini CSS nya wkwk
    Tolong bantu ini CSS nya wkwk

--- js/logo.js
+++ js/logo.js
@@ -5,11 +5,3 @@ $(window).scroll(function () {
         $('.logokomp').fadeOut('slow');
     }
 });
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() < 100) {
-        $('.logokom').fadeIn('slow');
-    } else {
-        $('.logokom').fadeOut('slow');
-    }
-});
---
+++ materi/materi.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="../matericss/all.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/commons_style-fc943f5d27.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/froala_style.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/academytutorials_style-de98efd511.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/botui.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/botui-theme-default.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/colors.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/ea731dcb6f.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/prettify.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/table.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/video.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/fullscreen.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/froala_editor.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/froala_style.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/fullscreen.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/image.min.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/geser.css" type="text/css" rel="stylesheet">
+    <link href="../matericss/line_breaker.min.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="../matericss/app.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+
+<body class="spy" data-spy="scroll" data-target="#spied">
+<div>
+<div class="content-wrapper">
+    <div class="list-modul">
+        <div class="container-fluid pt-5" id="geser">
+                    <div class="container">
+                        <div class="row mb-9">
+                            <div class="col-lg-4 d-none d-lg-block pb-5">
+                                <div class="sticky">
+                                            <div data-target="moduleSearch.result"></div>
+                                                <table class="table table-responsive-sm table-hover"
+                                                    data-target="moduleSearch.allModules">
+                                                    <tbody id="js-module-list">
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5039">Vuforia
+                                                                    Target Manager (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5044">Image
+                                                                    Target (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5049">Multi
+                                                                    Target (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5054">Model
+                                                                    Target (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5057">Mengoptimalkan
+                                                                    Deteksi Target dan Menstabilkan Pelacakan (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5060">Extended
+                                                                    Tracking (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5065">Modul
+                                                                    7: Overview Vuforia (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5070">Implementasi
+                                                                    Vuforia (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5075">Database
+                                                                    Vuforia (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5132">Modul
+                                                                    8: Augmented Reality untuk Media Sosial (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5145">Tips
+                                                                    dan Best Practice (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5150">Interface
+                                                                    Spark AR Studio (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5155">Membuat
+                                                                    Filter Sederhana (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5157">Membuat
+                                                                    Filter Sederhana - Lanjutan (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5160">Spark
+                                                                    AR Player (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr class="">
+                                                            <td scope="row"><a
+                                                                    href="https://www.dicoding.com/academies/135/tutorials/5152">Publish
+                                                                    ke Facebook dan Instagram (Gratis)</a></td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td scope="row"><a class="disabled">Submission: Augmented Reality </a>
+                                                            </td>
+                                                            <td class="text-center"></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                </div>
+                            </div>
+                        </div>
+        </div>
+    </div>
+    <div class="col-lg-9 p-lg-5 p-sm-3 shadow-sm border rounded">
+                        <div class="alert alert-success mb-5" role="alert">
+                            <div class="row">
+                                <div class="ml-3">
+                                </div>
+                            </div>
+                        </div>
+    </div>
+</div>
+</div>
+</body>
+
+</html>
